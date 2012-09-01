129047d329eb986e36a4a75858e9e202dcbd1c29
Cefyr <cefyr.mail@gmail.com>
2012-09-01 15:27:15 +0200

    Removed textpad pieces

--- halvdan.py
+++ halvdan.py
@@ -4,15 +4,12 @@
 # TEST AND THINGIES USING CURSES IN PYTHON
 
 import curses, re
-import curses.textpad as textpad
 
 def main(stdscr):
     # Dear old int main() :D
     h = 20
     w = 80
-#    scr = curses.newwin(h - 1, w, 0, 0) #might have to be a pad?
-    pad = textpad.Textbox(stdscr, insert_mode = True)
-    pad.stripspaces = False
+    scr = curses.newwin(h - 1, w, 0, 0) #might have to be a pad?
     bar = curses.newwin(1, w, h - 1, 0)
 
     on_scr = True
@@ -22,31 +19,27 @@ def main(stdscr):
     with open(filename) as f:
         text = f.read()
     
-    # Helpful regexes and list comprehendings should be kept an eye on
-    # so I don't forget how they work here
+    # Helpful regexes and things by nycz
     lines = re.split(r'(\n)', text)
     zippedlines = zip(lines[::2], lines[1::2] + [''])
     lines2 = [t + nl for t, nl in zippedlines]
     chunks = ''.join(lines2)
 
     for c in chunks:
-        stdscr.addstr(c)
+        scr.addstr(c)
     bar.addstr('>>> Status-bar whoop whoop >>>', curses.A_REVERSE)
-    stdscr.noutrefresh()
+    scr.noutrefresh()
     bar.noutrefresh()
     curses.doupdate()
 
-    editface = pad.edit()
-    stdscr.addstr(1,1,editface.encode('utf_8')) 
-    stdscr.refresh()
     
-#    while True:
-#        ch = scr.getch()
-#        if ch == ord('q'): break
-#                                              
-#        scr.noutrefresh()
-#        bar.noutrefresh()
-#        curses.doupdate()
+    while True:
+        ch = scr.getch()
+        if ch == ord('q'): break
+                                              
+        scr.noutrefresh()
+        bar.noutrefresh()
+        curses.doupdate()
 
 # This thingy is supposed to fix all the initscr() and noecho() and so on
 # Go, go, gadget-wrapper!
