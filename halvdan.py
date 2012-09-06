84bc6f6d486a7a184d60c63747f1dde9b37da339
Cefyr <cefyr.mail@gmail.com>
2012-09-06 22:38:38 +0200

    Added support for terminal size, fiddled with getch-loop because it cannot get any worse, tentative addition of function for inserting characters because it seemed a good idea at the time

--- halvdan.py
+++ halvdan.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# TEST AND THINGIES USING CURSES IN PYTHON
+# TEXT EDITOR THINGY USING CURSES IN PYTHON
 
-import curses, re
+import curses, locale, re
 
 def main(stdscr):
     # Dear old int main() :D
-    h = 20
-    w = 80
-    scr = curses.newwin(h - 1, w, 0, 0) #might have to be a pad?
+
+    try:
+        import terminal_info
+        w,h = terminal_info.terminal_size()
+    except:
+        w = 80
+        h = 24
+    ws = str(w)
+    hs = str(h)
+    scr = curses.newwin(h - 1, w, 0, 0) 
     bar = curses.newwin(1, w, h - 1, 0)
 
     on_scr = True
-    #cmd = {curses.KEY_F2:break}
+    cmd = {}
+#    cmd[str(229)] = insert_ch(scr, 'å')
 
     filename = 'test.txt'
     with open(filename) as f:
@@ -25,22 +33,45 @@ def main(stdscr):
     lines2 = [t + nl for t, nl in zippedlines]
     chunks = ''.join(lines2)
 
+    bar.addstr('>>> Status-bar H*W >>> ' + hs + '*' + ws, curses.A_REVERSE)
+
     for c in chunks:
         scr.addstr(c)
-    bar.addstr('>>> Status-bar whoop whoop >>>', curses.A_REVERSE)
     scr.noutrefresh()
     bar.noutrefresh()
     curses.doupdate()
 
     
     while True:
-        ch = scr.getch()
-        if ch == ord('q'): break
-                                              
+        ch = scr.getch() #NOTE ch is an int; chr(ch) returns string
+#        if ch == ord('q'): break
+        if on_scr and ch in cmd:
+            cmd[ch]
+        else:
+            if on_scr:
+                insert_ch(scr, chr(ch))
+#        else:
+#            # handle statusbar command
+#            pass
         scr.noutrefresh()
         bar.noutrefresh()
         curses.doupdate()
 
+
+def insert_ch(win, char):
+    win.addstr(char)            
+
+def exit():
+    pass
+
+
 # This thingy is supposed to fix all the initscr() and noecho() and so on
 # Go, go, gadget-wrapper!
+locale.setlocale(locale.LC_ALL, '')
 curses.wrapper(main)
+
+
+#TODO stop the evil ASCII?
+#TODO Enable move between status bar and screen
+#TODO Enable moving on screen
+#TODO Possibly think about putting a resize thingy among the refreshes
